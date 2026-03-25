87cf98cbf9809e518a683e49dd51fd0136db5a65
nguyenhoang136 <nguyenhoang13062004bg@gmail.com>
2026-03-25 20:04:17 +0700

    update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,7 +2,6 @@ import express from 'express';
 import { getCurrentUser, loginUser, registerUser, updatePassword, updateProfile } from '../controllers/userController.js';
 import authMiddleware from '../middleware/auth.js';
 
-
 const userRouter = express.Router();
 userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
